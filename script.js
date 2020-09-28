1f56537f282e86288b8ad661ae045576cad85928
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-09-28 00:31:15 -0400

    fade in effects partilly working

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        $('#fade-in').each(function(i){
+            var bottomOfObject = $(this).position().top + $(this).outerHeight();
+            var bottomOfWindow = $(window).scrollTop() + $(window).height();
+            if(bottomOfWindow > bottomOfObject){
+                $(this).animate({'opacity': '1'}, 1500);
+            }
+        })
+    })
+})
