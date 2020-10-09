dcfb3d27f7df5b532b35a28d40c2e06baf837717
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-10-08 20:20:14 -0400

    card fade in animation changes

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
         $('.col-sm-6').each(function(i){
             var bottomOfObject = $(this).position().top + $(this).outerHeight();
             var bottomOfWindow = $(window).scrollTop() + $(window).height();
-            if(bottomOfWindow > bottomOfObject){
-                $(this).animate({'opacity': '1'}, 500);
+            if(bottomOfWindow > bottomOfObject - 300){
+                $(this).animate({'opacity': '1'}, 1000);
             }
         })
     })
